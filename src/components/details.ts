92dedc3c8d78886b51b3cc3e05671af2bc266761
Emerson <emerzon.lc@gmail.com>
2023-07-13 11:09:28 -0500

    fetaure:loading

--- src/components/details.ts
+++ src/components/details.ts
@@ -15,27 +15,77 @@ import {
   imagesAllGames,
 } from "../utilities/fetchGetGames";
 
+const loading = createDOM(`
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          style="margin: auto; display: block; shape-rendering: auto"
+          width="100px"
+          height="100px"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid"
+        >
+          <circle
+            cx="50"
+            cy="50"
+            fill="none"
+            stroke="#7d7d7d"
+            stroke-width="10"
+            r="35"
+            stroke-dasharray="164.93361431346415 56.97787143782138"
+          >
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              repeatCount="indefinite"
+              dur="1s"
+              values="0 50 50;360 50 50"
+              keyTimes="0;1"
+            />
+          </circle>
+        </svg>
+      `);
+
+const loaderEl = document.querySelector("#loaderDetails") as HTMLElement;
+loaderEl.append(loading);
+
 async function getDetails(): Promise<void> {
-  const detailGames = await getDetailsbyGameSlug();
-  const creatorsGames = await getDetailsCreatorsbyGameSlug();
-  const achievementsGames = await getFirstAchievementsOfGames();
-  const postGames = await getFirstPostOfGames();
-  const imagesGames = await imagesAllGames();
-  if (
-    !detailGames ||
-    !creatorsGames ||
-    !achievementsGames ||
-    !postGames ||
-    !imagesGames
-  )
-    return;
-  getEachGameDetail(
-    detailGames,
-    creatorsGames,
-    achievementsGames,
-    postGames,
-    imagesGames
-  );
+  try {
+    //show loader//
+    const [
+      detailGames,
+      creatorsGames,
+      achievementsGames,
+      postGames,
+      imagesGames,
+    ] = await Promise.all([
+      getDetailsbyGameSlug(),
+      getDetailsCreatorsbyGameSlug(),
+      getFirstAchievementsOfGames(),
+      getFirstPostOfGames(),
+      imagesAllGames(),
+    ]);
+
+    if (
+      !detailGames ||
+      !creatorsGames ||
+      !achievementsGames ||
+      !postGames ||
+      !imagesGames
+    )
+      return;
+    getEachGameDetail(
+      detailGames,
+      creatorsGames,
+      achievementsGames,
+      postGames,
+      imagesGames
+    );
+  } catch (error) {
+    console.log("error", error);
+  } finally {
+    loading.remove();
+  }
 }
 
 async function getEachGameDetail(
--- src/utilities/fetchGetGames.ts
+++ src/utilities/fetchGetGames.ts
@@ -1,4 +1,5 @@
 const API_KEY = "2b650dbbb0f64479b2f2bf63a1d29c13";
+import { createDOM } from "../utilities/stringToDOM";
 export type ParentPlatforms = {
   platform: {
     id: number;
@@ -237,6 +238,40 @@ export type descriptionGameGenreType = {
   description: string;
 };
 
+const loadingHome = createDOM(`
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          style="margin: auto; display: block; shape-rendering: auto"
+          width="100px"
+          height="100px"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid"
+        >
+          <circle
+            cx="50"
+            cy="50"
+            fill="none"
+            stroke="#7d7d7d"
+            stroke-width="10"
+            r="35"
+            stroke-dasharray="164.93361431346415 56.97787143782138"
+          >
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              repeatCount="indefinite"
+              dur="1s"
+              values="0 50 50;360 50 50"
+              keyTimes="0;1"
+            />
+          </circle>
+        </svg>
+      `);
+
+const loaderHomeEl = document.querySelector("#loadingHome") as HTMLElement;
+loaderHomeEl.append(loadingHome);
+
 export async function getAllGames(): Promise<GetAllGamesResponse | undefined> {
   try {
     const response = await fetch(
@@ -246,6 +281,8 @@ export async function getAllGames(): Promise<GetAllGamesResponse | undefined> {
     return data;
   } catch (err) {
     console.log("Hubo un error en el fetch");
+  } finally {
+    loadingHome.remove();
   }
 }
 
